27e89adf029a9b6a3234cd1dc281f26705d1fa34
JyotiSharma26 <145347047+JyotiSharma26@users.noreply.github.com>
2025-01-19 08:23:13 -0800

    Time: 27 ms (23.28%), Space: 46.3 MB (65.55%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,37 +1,38 @@
 class Solution {
 
     public int trapRainWater(int[][] heightMap) {
-        //left,right,down, up
-        int dir[][] = {{0,-1},{0,1},{-1,0},{1,0}};
+        // left,right,down, up
+        int dir[][] = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
         int rows = heightMap.length;
         int cols = heightMap[0].length;
-        if(rows <3 || cols <3) return 0;
-        int totalUnvisitedCells = rows*cols;
+        if (rows < 3 || cols < 3)
+            return 0;
+        int totalUnvisitedCells = rows * cols;
         boolean[][] visited = new boolean[rows][cols];
         // [hight, row, col] -> inc order of height
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0]-b[0]));
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+
         for (int i = 0; i < rows; i++) {
-            pq.offer(new int[]{heightMap[i][0], i, 0});
-            pq.offer(new int[]{heightMap[i][cols - 1], i, cols - 1});
+            pq.offer(new int[] { heightMap[i][0], i, 0 });
+            pq.offer(new int[] { heightMap[i][cols - 1], i, cols - 1 });
             visited[i][0] = true;
             visited[i][cols - 1] = true;
             totalUnvisitedCells--;
             totalUnvisitedCells--;
         }
 
-        for (int i = 1; i < cols-1; i++) {
-            pq.offer(new int[]{heightMap[0][i], 0, i});
-            pq.offer(new int []{heightMap[rows - 1][i], rows - 1, i});
+        for (int i = 1; i < cols - 1; i++) {
+            pq.offer(new int[] { heightMap[0][i], 0, i });
+            pq.offer(new int[] { heightMap[rows - 1][i], rows - 1, i });
             visited[0][i] = true;
             visited[rows - 1][i] = true;
             totalUnvisitedCells--;
             totalUnvisitedCells--;
         }
         int trappedWater = 0;
-        int waterLevel=0;
-        //n*mlog(n*m)
-        while (!pq.isEmpty() && totalUnvisitedCells>0) {
+        int waterLevel = 0;
+        // n*mlog(n*m)
+        while (!pq.isEmpty() && totalUnvisitedCells > 0) {
             int currentCell[] = pq.poll();
             int currentHeight = currentCell[0];
             int currentRow = currentCell[1];
@@ -42,12 +43,12 @@ class Solution {
                 int neighborRow = currentRow + dir[direction][0];
                 int neighborCol = currentCol + dir[direction][1];
                 // Check if the neighbor is within the grid bounds and not yet visited
-                if (isValidCell(neighborRow,neighborCol,rows,cols) && !visited[neighborRow][neighborCol]) {
+                if (isValidCell(neighborRow, neighborCol, rows, cols) && !visited[neighborRow][neighborCol]) {
                     int neighborHeight = heightMap[neighborRow][neighborCol];
                     if (neighborHeight < waterLevel) {
                         trappedWater += waterLevel - neighborHeight;
                     }
-                    pq.offer(new int[]{neighborHeight,neighborRow,neighborCol});
+                    pq.offer(new int[] { neighborHeight, neighborRow, neighborCol });
                     visited[neighborRow][neighborCol] = true;
                     totalUnvisitedCells--;
                 }
@@ -56,7 +57,7 @@ class Solution {
         return trappedWater;
     }
 
-    private boolean isValidCell(int row,int col,int rows,int cols) {
+    private boolean isValidCell(int row, int col, int rows, int cols) {
         return row >= 0 && col >= 0 && row < rows && col < cols;
     }
 }
